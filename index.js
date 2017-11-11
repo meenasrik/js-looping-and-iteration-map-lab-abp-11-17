64fe23ea54f00359a2f94175af0fcd7272e15f3c
Meenakshi Radhakrishnan <junnamma@yahoo.com>
2017-11-11 18:23:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file.
+function lowerCaseDrivers (arr) {
+  return arr.map(function (name) {
+    return name.toLowerCase();
+  });
+}
+
+function nameToAttributes (arr) {
+  return arr.map(function (name) {
+    const fname = name.split(' ')[0];
+    const lname = name.split(' ')[1];
+    return {firstName: fname, lastName: lname};
+  });
+}
+
+function attributesToPhrase (arr) {
+  return arr.map(function (obj) {
+    const dname = obj.name;
+    const dhometown = obj.hometown;
+    return `${dname} is from ${dhometown}`;
+  });
+}
